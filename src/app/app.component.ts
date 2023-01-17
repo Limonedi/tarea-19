b3038ff158e03eaa407833fd6d767331bb1198be
Limonedi <holuloldoce@gmail.com>
2023-01-16 23:45:30 -0500

    Actualización final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { PokedexService } from './services/pokedex.service';
 
 
 @Component({
@@ -9,13 +10,43 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'tarea19';
-  pokemon: any;
-  searchTerm!: string;
+  data:any[] = [];
+  searchTerm!:string;
+  pokemons:any[]=[];
 
-  constructor(private http: HttpClient) {}
-  searchPokemon() {
-    this.http.get(`https://pokeapi.co/api/v2/pokemon/${this.searchTerm}`).subscribe((data) => {
-      this.pokemon = data;
-    });
+  constructor(private pokedex:PokedexService){}
+
+  ngOnInit():void{
+    this.getPokemons()
   }
+  searchPokemon() {
+    this.data = this.data.filter(pokemon => {
+        return pokemon.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+               pokemon.orden.toString().includes(this.searchTerm);
+    });
 }
+resetSearch() {
+  this.data = [...this.pokemons]; // o this.getPokemons() si prefieres volver a hacer la petición a la API
+  this.searchTerm = "";
+}
+
+    
+getPokemons(){
+  let pokemonData;
+  for(let i = 1; i < 150; i++){
+      this.pokedex.getPokemon(String(i)).subscribe(
+          res => {
+              pokemonData = {
+                  position: i,
+                  image: res.sprites.front_default,
+                  name: res.name,
+                  orden: res.order
+              }
+              this.data.push(pokemonData)
+              this.pokemons = [...this.data];
+          },
+          err => {
+          }
+      )
+  }
+}}
---
+++ src/app/services/pokedex.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokedexService {
+
+  urlBase="https://pokeapi.co/api/v2";
+
+  constructor(private http:HttpClient) { 
+    console.log('Pokedex')
+  }
+
+
+  getPokemon(index: string){
+    return this.http.get<any>(`${this.urlBase}/pokemon/${index}`);
+  }}
